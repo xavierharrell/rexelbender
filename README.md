8555bd8f573e755f3d1248cecb2a5643290af9d2
Xavier Harrell <xavierharrell@gmail.com>
2014-06-04 23:05:02 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+xavierharrell
+=============
+
+My website
---
+++ js/min/scripts-ck.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(t){t(document).foundation();var e=t(".port-container").isotope({itemSelector:".panel",layoutMode:"fitRows",animationEngine:"best-available"});t(".filter-select").on("click","li a",function(){var i=t(this).attr("data-filter");return e.isotope({filter:i}),!1})});
