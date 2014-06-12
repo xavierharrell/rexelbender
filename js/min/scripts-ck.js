b7b31158a6856306a7bc3f547b4c832fb6b327d9
Xavier Harrell <xavierharrell@gmail.com>
2014-06-12 07:01:30 -0500

    New start. Removed old site specs and started over

--- js/min/scripts-ck.js
+++
@@ -1,1 +0,0 @@
-jQuery(document).ready(function(t){t(document).foundation();var e=t(".port-container").isotope({itemSelector:".panel",layoutMode:"fitRows",animationEngine:"best-available"});t(".filter-select").on("click","li a",function(){var i=t(this).attr("data-filter");return e.isotope({filter:i}),!1})});
