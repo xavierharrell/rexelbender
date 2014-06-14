c3681c4a5509cee84b53c0ab408ac9e1b23c79e1
Xavier A. Harrell <xavier@Xaviers-Mac-Pro.local>
2014-06-13 20:48:28 -0500

    Removed sass cache.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-$(document).foundation();
+jQuery(document).ready(function($) {
+    //Foundation Init
+    $(document).foundation();
+
+    var $container = $('.port-container').isotope({
+    itemSelector: '.port-item',
+    layoutMode: 'fitRows',
+    animationEngine: 'best-available'
+                                       });
+
+  // bind filter button click
+  $('.filter-select').on( 'click', 'li a', function() {
+    var filterValue = $( this ).attr('data-filter');
+    // use filterFn if matches value
+    $container.isotope({filter: filterValue });
+        return false;
+  });
+
+
+});//End of Scripts
