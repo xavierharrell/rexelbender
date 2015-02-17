9beac4736d82b2b141c46f8244b55186a243bb70
Xavier Harrell <xavierharrell@gmail.com>
2015-02-17 15:42:44 -0600

    Added new task output

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,6 +1,7 @@
 var changed    = require('gulp-changed');
 var gulp       = require('gulp');
 var imagemin   = require('gulp-imagemin');
+// var svg2png	   = require('gulp-svg2png');
 var config     = require('../config').images;
 var browserSync  = require('browser-sync');
 
@@ -8,6 +9,7 @@ gulp.task('images', function() {
   return gulp.src(config.src)
     .pipe(changed(config.dest)) // Ignore unchanged files
     .pipe(imagemin()) // Optimize
+    // .pipe(svg2png())
     .pipe(gulp.dest(config.dest))
     .pipe(browserSync.reload({stream:true}));
 });
