3dac9f18ffe475740a5b44a20dba9bf034fd00e2
Xavier Harrell <xavierharrell@gmail.com>
2015-06-22 10:49:01 -0500

    change to gulp

--- gulp/config.js
+++ gulp/config.js
@@ -50,13 +50,8 @@ module.exports = {
         options: {
             progressive: true,
             multipass: true,
-            svgoPlugins: [{
-                removeUselessStrokeAndFill: false
-            }, {
-                removeViewBox: false
-            }, {
-                cleanupIDs: true
-            }]
+            svgoPlugins: [
+            {removeUnknownsAndDefaults: false}]
         }
     },
     svgpng: {
