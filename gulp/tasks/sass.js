98e7886aaf8e890515d39ee352636bc31019c001
Xavier Harrell <xavierharrell@gmail.com>
2016-10-15 12:36:28 -0500

    prepped before changing structure

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -8,11 +8,11 @@ var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
   return gulp.src(config.src)
+          // .pipe(autoprefixer({ browsers: ['last 2 version'], cascade: false }))
     .pipe(sourcemaps.init())
     .pipe(sass(config.settings))
     .on('error', handleErrors)
     .pipe(sourcemaps.write())
-    .pipe(autoprefixer({ browsers: ['last 2 version'] }))
     .pipe(gulp.dest(config.dest))
     .pipe(browserSync.reload({stream:true}));
 });
